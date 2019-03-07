8d6a28d17137338361618bf6f92658fffc2ca910
Liam Doran <liamfd@gmail.com>
2019-03-07 14:41:34 -0500

    chore: create minimal repro issue

---
+++ src/pages/page-3/index.js
@@ -0,0 +1,3 @@
+import HelloWorld from '../../components/hello-world';
+
+export default HelloWorld;
---
+++ src/pages/page-4/index.js
@@ -0,0 +1,5 @@
+/**
+ * `yarn gatsby build` should fail with this here. Compare to ../page-3,
+ * which is functionally identical.
+ */
+export { default } from '../../components/hello-world';
